fdbfe0c2ae4ed6c24ae3e41ccad9b070faa8792d
kassehpoor <kasehpoor.k@gmail.com>
2019-08-07 15:20:21 +0430

    function component

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -6,54 +6,42 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-// Square component is a controlled component.The Board has full control over them.
-var Square = function (_React$Component) {
-  _inherits(Square, _React$Component);
-
-  function Square(props) {
-    _classCallCheck(this, Square);
-
-    var _this = _possibleConstructorReturn(this, (Square.__proto__ || Object.getPrototypeOf(Square)).call(this, props));
-
-    _this.state = {
-      value: null
+/*
+class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
     };
-    return _this;
   }
 
-  _createClass(Square, [{
-    key: 'render',
-    value: function render() {
-      var _this2 = this;
+  render() {
+    return (
+      <button
+        className="square"
+        onClick={() => this.props.onClick()}
+      >
+        {this.props.value}
+      </button>
+    );
+  }
+}
+*/
+// Square component is a controlled component.The Board has full control over them.
+function Square(props) {}
 
-      return React.createElement(
-        'button',
-        {
-          className: 'square',
-          onClick: function onClick() {
-            return _this2.props.onClick();
-          }
-        },
-        this.props.value
-      );
-    }
-  }]);
-
-  return Square;
-}(React.Component);
-
-var Board = function (_React$Component2) {
-  _inherits(Board, _React$Component2);
+var Board = function (_React$Component) {
+  _inherits(Board, _React$Component);
 
   function Board(props) {
     _classCallCheck(this, Board);
 
-    var _this3 = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
 
-    _this3.state = {
+    _this.state = {
       squares: Array(9).fill(null)
     };
-    return _this3;
+    return _this;
   }
 
   _createClass(Board, [{
@@ -67,12 +55,12 @@ var Board = function (_React$Component2) {
   }, {
     key: 'renderSquare',
     value: function renderSquare(i) {
-      var _this4 = this;
+      var _this2 = this;
 
       return React.createElement(Square, {
         value: this.state.squares[i],
         onClick: function onClick() {
-          return _this4.handleClick(i);
+          return _this2.handleClick(i);
         }
       });
     }
@@ -117,8 +105,8 @@ var Board = function (_React$Component2) {
   return Board;
 }(React.Component);
 
-var Game = function (_React$Component3) {
-  _inherits(Game, _React$Component3);
+var Game = function (_React$Component2) {
+  _inherits(Game, _React$Component2);
 
   function Game() {
     _classCallCheck(this, Game);
--- tic-tac-toe/src/app.js
+++ tic-tac-toe/src/app.js
@@ -1,4 +1,5 @@
-// Square component is a controlled component.The Board has full control over them.
+
+/*
 class Square extends React.Component {
   constructor(props) {
     super(props);
@@ -18,20 +19,36 @@ class Square extends React.Component {
     );
   }
 }
+*/
+// Square component is a controlled component.The Board has full control over them.
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
 
 class Board extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     };
   }
 
   handleClick(i) {
     //create a copy of squares to modify.instead of modyfiying the exsisting array.
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({ squares: squares });
+    if (calculateWinner(squares)||squares[i]){
+      return;
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -44,7 +61,13 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner){
+      status = 'Winner:' + winner;
+    }else {
+      status = 'Next player:'+(this.state.xIsNext ? 'X':'O');
+    }
 
     return (
       <div>
@@ -83,6 +106,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
